5ee6d19e2c8cab3586c68b8653dec88f55d6b398
abu fawwaz <tedisupriyadi@gmail.com>
2022-12-24 09:39:41 +0700

    menambahkan menu transfer file

---
+++ app/src/main/java/com/roaita/imsakiyah/file/TransferApp.java
@@ -0,0 +1,12 @@
+
+package com.roaita.imsakiyah.file;
+
+import android.app.Application;
+
+
+public class TransferApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/roaita/imsakiyah/file/util/WifiUtils.java
@@ -0,0 +1,39 @@
+package com.roaita.imsakiyah.file.util;
+
+import android.util.Log;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+/**
+ * @author chris
+ */
+public class WifiUtils {
+    private static final String TAG = "WifiUtils";
+
+    public static String getDeviceIpAddress() {
+        String deviceIpAddress = "###.###.###.###";
+
+        try {
+            for (Enumeration<NetworkInterface> enumeration =
+                 NetworkInterface.getNetworkInterfaces(); enumeration.hasMoreElements(); ) {
+                NetworkInterface networkInterface = enumeration.nextElement();
+
+                for (Enumeration<InetAddress> enumerationIpAddr =
+                     networkInterface.getInetAddresses(); enumerationIpAddr.hasMoreElements(); ) {
+                    InetAddress inetAddress = enumerationIpAddr.nextElement();
+
+                    if (!inetAddress.isLoopbackAddress() && inetAddress.getAddress().length == 4) {
+                        deviceIpAddress = inetAddress.getHostAddress();
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            Log.e(TAG, "SocketException:" + e.getMessage());
+        }
+
+        return deviceIpAddress;
+    }
+}
